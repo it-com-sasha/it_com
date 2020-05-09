08cec7a3065a25c64b7f783d65a2443d94a77069
it-com-sasha <in-mg@ukr.net>
2020-05-09 13:43:04 +0300

    Первый коммит

---
+++ pages/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class TemplateClass(models.Model):
+    title = models.CharField('название', max_length=150)
+    description = models.TextField('описание', blank=True)
+    slug = models.SlugField('чпу', unique=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Page(TemplateClass):
+    menu = models.ManyToManyField('Menu', verbose_name='меню', blank=True)
+    categories = models.ManyToManyField('Category', verbose_name='категория')
+    image = models.ImageField('изображение', upload_to='page_images/', height_field=None, width_field=None, max_length=100, blank=True)
+    draft = models.BooleanField('черновик', default=False)
+    
+
+    class Meta:
+	    verbose_name = "Страница"
+	    verbose_name_plural = "Страницы"
+
+class Category(TemplateClass):
+    
+    class Meta:
+	    verbose_name = "Категория"
+	    verbose_name_plural = "Категории"
+
+class Menu(TemplateClass):
+    slug = None
+    
+    class Meta:
+	    verbose_name = "Меню"
+	    verbose_name_plural = "Меню"
---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+
+from .models import Page
+
+class PagesView(View):
+    def get(self, request):
+        pages = Page.objects.all()
+        return render(request, 'pages/pages_list.html', {})
+        
+        
+    
