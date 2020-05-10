30a97fbddbfe1446ca9f1b34e5bb86ed30f12881
it-com-sasha <in-mg@ukr.net>
2020-05-10 21:03:43 +0300

    Разрабока приложения pages

--- it_com/settings.py
+++ it_com/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'it_com.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- pages/views.py
+++ pages/views.py
@@ -6,7 +6,10 @@ from .models import Page
 class PagesView(View):
     def get(self, request):
         pages = Page.objects.all()
-        return render(request, 'pages/pages_list.html', {})
+        return render(request, 'pages/pages_list.html', {'pages_list': pages})
         
-        
-    
+
+
+
+
+
